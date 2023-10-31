77955ec63a12bc9a1aba9f2cea64a6e9cca7cf9d
Jeong <penicillinj@naver.com>
2023-10-31 15:52:50 +0900

    fix: text info

--- mod_basic.py
+++ mod_basic.py
@@ -64,6 +64,7 @@ class ModuleBasic(PluginModuleBase):
                 img = Image.open(BytesIO(img_bytes))
                 img.save(filepath)
                 img_url = SupportDiscord.discord_proxy_image_localfile(filepath)
+                logger.info(img_url)
                 msg = '테스트'
                 msg += f"\n예치금 : {data['deposit']}"
                 msg += f"\n이미 구입 : {data['history']['count']}건 (미추첨)"
